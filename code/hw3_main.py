7fdff18251dc70f0a7369c633df76351153f45e0
Yifan Wu <yiw084@ucsd.edu>
2021-02-28 01:32:43 -0500

    choose data 1

--- code/hw3_main.py
+++ code/hw3_main.py
@@ -1,20 +1,42 @@
+import time
+
 import numpy as np
+from tqdm import tqdm
+from numba import jit
+
 from utils import *
 
-if __name__ == '__main__':
-    # only choose ONE of the following data
 
-    # data 1. this data has features, use this if you plan to skip the extra credit feature detection and tracking part
+def tic(message=None):
+    if message:
+        print(message)
+    else:
+        print("############ Time Start ############")
+    return time.time()
+
+
+def toc(tstart, name="Operation"):
+    print(f'############ {name} took: {(time.time() - tstart):.4f} sec. ############\n')
+
+
+def main():
+    '''
+        data 2. this data does NOT have features, you need to do feature detection and tracking but will receive extra
+        credit filename = "./data/03.npz"
+        t,features,linear_velocity,angular_velocity,K,b,imu_T_cam = load_data(filename)
+    '''
+    pass
+
+
+if __name__ == '__main__':
+    '''
+    data 1. this data has features, use this if you plan to skip the extra credit feature detection and tracking part
+    '''
     filename = "./data/10.npz"
     t, features, linear_velocity, angular_velocity, K, b, imu_T_cam = load_data(filename, load_features=True)
 
-    # data 2. this data does NOT have features, you need to do feature detection and tracking but will receive extra credit
-    # filename = "./data/03.npz"
-    # t,features,linear_velocity,angular_velocity,K,b,imu_T_cam = load_data(filename)
-
     # (a) IMU Localization via EKF Prediction
 
-    # (b) Feature detection and matching
 
     # (c) Landmark Mapping via EKF Update
 
